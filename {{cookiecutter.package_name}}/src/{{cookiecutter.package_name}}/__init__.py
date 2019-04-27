6e13e6f93b37cf91fbf6b2a59a0f2d74aa5a04ab
Germione <germione.grangere@gmail.com>
2019-04-27 20:46:26 +0200

    feat: first version of package template

---
+++ {{cookiecutter.package_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -0,0 +1,14 @@
+
+__version__ = "{{cookiecutter.package_version}}"
+
+__title__ = "{{cookiecutter.package_name}}"
+__description__ = "{{cookiecutter.package_description}}"
+__url__ = "{{cookiecutter.package_url}}"
+
+__author__ = "{{cookiecutter.package_author}}"
+__author_email__ = "{{cookiecutter.package_author_email}}"
+
+__license__ = "{{cookiecutter.package_license}}"
+
+def main():
+    print("Using {{cookiecutter.package_name}}")
