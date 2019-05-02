949a7a0e925d2e0aa7ecfec3d9e9505736e46e15
Germione <germione.grangere@gmail.com>
2019-05-02 10:35:40 +0200

    feat: add NO CLI option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,12 +1,20 @@
 import os
 
 def del_license_file():
-    print("Deleting LICENSE file")
     os.unlink("LICENSE")
+
+def del_cli_file():
+    os.unlink("src/{{cookiecutter.package_name}}/cli.py")
+    os.unlink("tests/test_cli.py")
 
 if __name__ == "__main__":
 # Be careful with whitespace control from jinja
 {% if cookiecutter.package_license == "No License" %}
     del_license_file()
 {% endif %}
+
+{% if cookiecutter.package_use_cli == "No command line" %}
+    del_cli_file()
+{% endif %}
+
     print("Cookiecutter succeeded to create <{{cookiecutter.package_name}}>")
